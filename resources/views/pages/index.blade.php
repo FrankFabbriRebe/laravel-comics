f36ad049751f169cec4c5a6cadf6c6ef4cd38e1d
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2024-02-09 16:20:56 +0100

    include: component

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main-layout')
 @section('head')
-    <title>Home</title>
+<title>Comic</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+@include('components.card_container')
 @endsection
